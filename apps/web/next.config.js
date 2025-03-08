34f3392af156baddcd404d926db5e5b241bb1cc6
zzzzshawn <zzzzshawn@gmail.com>
2025-03-09 01:50:49 +0530

    ngmi

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-fd21440a971545968d1aa765101f1c1e.r2.dev',
+        port: '',
+        pathname: '/apollo/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig 
--- apps/web/src/components/ImageGrid/ImageCard.tsx
+++ apps/web/src/components/ImageGrid/ImageCard.tsx
@@ -113,90 +113,91 @@ export function ImageCard({
   }
 
   return (
-      <div className=" w-full aspect-square gap-1.5 rounded-xl relative flex flex-col p-0.5 flex-shrink">
+    <div className=" w-full aspect-square gap-1.5 rounded-xl relative flex flex-col p-0.5 flex-shrink">
 
-        {imgUrl && (
-          <div
-            className="aspect-[1] size-full cursor-pointer select-none border rounded-2xl p-0.5 relative  bg-dark-2/20 backdrop-blur-md backdrop-saturate-100"
-            onClick={() => {
-              setData({
-                prompt,
-                imageUrl: imgUrl,
-              })
-            }}
-          >
-            <Image
-              alt={prompt ?? "image"}
-              src={imgUrl}
-              loading="lazy"
-              width={1000}
-              height={1000}
-              className={cn(
-                "pointer-events-none size-full rounded-xl object-cover object-top",
-                {
-                  "blur-md": isImageLoading,
-                },
-              )}
-              onLoad={() => setIsImageLoading(false)}
-            />
+      {imgUrl && (
+        <div
+          className="aspect-[1] size-full cursor-pointer select-none border rounded-2xl p-0.5 relative  bg-dark-2/20 backdrop-blur-md backdrop-saturate-100"
+          onClick={() => {
+            setData({
+              prompt,
+              imageUrl: imgUrl,
+            })
+          }}
+        >
+          <Image
+            alt={prompt ?? "image"}
+            src={imgUrl}
+            loading="lazy"
+            width={1000}
+            height={1000}
+            unoptimized
+            className={cn(
+              "pointer-events-none size-full rounded-xl object-cover object-top",
+              {
+                "blur-md": isImageLoading,
+              },
+            )}
+            onLoad={() => setIsImageLoading(false)}
+          />
 
-            <div className="absolute px-1.5 rounded-xl w-full bottom-1.5 left-1/2 -translate-x-1/2 cursor-auto">
-              <div className="relative p-0.5 rounded-xl backdrop-blur-md backdrop-saturate-100 bg-neutral-900/30">
-                <div className="relative p-0.5 px-1.5 rounded-xl flex flex-col sm:gap-0.5">
-                  <div className="flex items-center justify-between gap-0.5">
+          <div className="absolute px-1.5 rounded-xl w-full bottom-1.5 left-1/2 -translate-x-1/2 cursor-auto">
+            <div className="relative p-0.5 rounded-xl backdrop-blur-md backdrop-saturate-100 bg-neutral-900/30">
+              <div className="relative p-0.5 px-1.5 rounded-xl flex flex-col sm:gap-0.5">
+                <div className="flex items-center justify-between gap-0.5">
 
-                    <p className="max-sm:text-xs text-white">Prompt</p>
-                    <Button
-                      size={"icon"}
-                      title="Copy to clipboard"
-                      className="size-6 bg-transparent hover:!bg-transparent shadow-none outline-none"
-                      onClick={onCopy}
+                  <p className="max-sm:text-xs text-white">Prompt</p>
+                  <Button
+                    size={"icon"}
+                    title="Copy to clipboard"
+                    className="size-6 bg-transparent hover:!bg-transparent shadow-none outline-none"
+                    onClick={onCopy}
 
-                    >
-                      {hasCheckIcon ? (
-                        <CheckIcon className="!size-3 text-white" />
-                      ) : (
-                        <Copy className="!size-3 text-white" />
-                      )}
-                    </Button>
-                  </div>
-                  <p className={cn("max-sm:text-xs", { truncate: truncate })}>{prompt}</p>
-
+                  >
+                    {hasCheckIcon ? (
+                      <CheckIcon className="!size-3 text-white" />
+                    ) : (
+                      <Copy className="!size-3 text-white" />
+                    )}
+                  </Button>
                 </div>
+                <p className={cn("max-sm:text-xs", { truncate: truncate })}>{prompt}</p>
+
               </div>
             </div>
           </div>
-        )}
+        </div>
+      )}
 
 
 
-        {imgUrl && isShareable && (
-          <div className="flex items-center justify-center gap-1 text-gray-600 border rounded-xl row-start-1 row-end-3 p-0.5  bg-dark-2/20 backdrop-blur-md backdrop-saturate-100">
-            <Button
-              variant={"ghost"}
-              size={"icon"}
-              title="Download"
-              className="animate-jelly w-full border rounded-xl bg-dark-2"
-              onClick={onDownload}
-            >
-              <Download className="!size-4 text-white" />
-            </Button>
-            <Button
-              variant={"ghost"}
-              size={"icon"}
-              title="Share"
-              className="animate-jelly w-full border rounded-xl bg-dark-2"
-              onClick={onShare}
-            >
-              <Share2 className="!size-4 text-white" />
-            </Button>
+      {imgUrl && isShareable && (
+        <div className="flex items-center justify-center gap-1 text-gray-600 border rounded-xl row-start-1 row-end-3 p-0.5  bg-dark-2/20 backdrop-blur-md backdrop-saturate-100">
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            title="Download"
+            className="animate-jelly w-full border rounded-xl bg-dark-2"
+            onClick={onDownload}
+          >
+            <Download className="!size-4 text-white" />
+          </Button>
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            title="Share"
+            className="animate-jelly w-full border rounded-xl bg-dark-2"
+            onClick={onShare}
+          >
+            <Share2 className="!size-4 text-white" />
+          </Button>
 
-          </div>
-        )}
-
-
+        </div>
+      )}
 
 
-      </div>
+
+
+    </div>
   )
 }
